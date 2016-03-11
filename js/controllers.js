3d69a053fd29dd5de06f3b42f2ce173e80133177
amit-wohlig <amit.wohlig@gmail.com>
2016-03-11 15:49:05 +0530

    done

--- js/controllers.js
+++ js/controllers.js
@@ -16,6 +16,26 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
   ];
+  $scope.client = [
+    {
+      img: "img/team.jpg",
+      name: "Jara visariya",
+      desg: "Product Manager, TATA Honeywell",
+      descp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+    },
+    {
+      img: "img/team.jpg",
+      name: "Jara visariya",
+      desg: "Product Manager, TATA Honeywell",
+      descp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+    },
+    {
+      img: "img/team.jpg",
+      name: "Jara visariya",
+      desg: "Product Manager, TATA Honeywell",
+      descp: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+    }
+  ];
 })
 
 .controller('headerctrl', function($scope, TemplateService) {
