cc2a13712c26176a210a9067e2dd05f50d5b27f5
Midhet <gadarohan17@gmail.com>
2016-04-23 13:18:20 +0530

    basic validation start

--- js/controllers.js
+++ js/controllers.js
@@ -44,6 +44,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   TemplateService.header = "views/content/header.html";
   $scope.formData = {};
   $scope.nominee = true;
+  $scope.nominees = {};
   $scope.birthDay = [
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
     "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
